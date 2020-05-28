d648209061d554d6bdaedb8b99a6c88691d0706c
Dhiru Sarania <dhirusarania@users.noreply.github.com>
2020-05-28 09:41:13 +0530

    my

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
+    <button @click="addToCount">count {{count}}</button>
   </div>
 </template>
 
+
+<script lang="ts">
+import { Vue, Component, Inject, Prop, Provide, Emit } from 'vue-property-decorator'
+
+@Component
+export default class Index extends Vue {
+  user = {
+    firstName: "sdsd",
+    lastName: 111,
+  }
+
+  foo:string = ""
+
+  count:number = 0
+
+  message: string = 'This is a message'
+
+  mounted(){
+    console.log("sdsd")
+    console.log(this.fullName)
+  }
+
+  get fullName (): string {
+    return `${this.user.firstName} ${this.user.lastName}`
+  }
+
+  // @Emit()
+  addToCount() {
+    this.count += 1
+  }
+}
+</script>
 
 <style scoped>
 
